b5d44eb1554c5c2ecd8eb7550c248560b3fe25bc
Kishan Patel <kishanpatel3612@gmail.com>
2025-04-10 00:14:23 +0530

    first time commit

---
+++ 01_Basic of Js/01_variable.js
@@ -0,0 +1,10 @@
+let name = "Alice";
+const age = 25;
+var isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is student:", isStudent);
+
+// age = 26; 
+name = "Bob";
---
+++ 01_Basic of Js/02_dataTypes.js
@@ -0,0 +1,17 @@
+let str = "Hello";       
+let num = 42;            
+let bool = false;         
+let und;                    
+let n = null;          
+let obj = { a: 1, b: 2 };
+let arr = [1, 2, 3];    
+let func = function() {};  
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof und);
+console.log(typeof n);    
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof func);
